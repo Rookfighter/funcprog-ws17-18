3c87ac7c399af832ee20247acae957ccfc5b9f8e
Fabian Meyer <user8324@posteo.net>
2017-10-19 10:41:56 +0200

    Move function defs above their implementation

--- ex01/src/StackCalc.hs
+++ ex01/src/StackCalc.hs
@@ -9,41 +9,44 @@ module StackCalc (
     readCommand
 ) where
 
-import Data.List
-
-pop  :: [Int] -> [Int]
-dup  :: [Int] -> [Int]
-add  :: [Int] -> [Int]
-sub  :: [Int] -> [Int]
-mul  :: [Int] -> [Int]
-neg  :: [Int] -> [Int]
-push :: [Int] -> Int -> [Int]
-readCommand :: String -> [Int] -> [Int]
-
 -- remove the last element from the stack
+pop  :: [Int] -> [Int]
 pop [] = []
 pop s  = init s
+
 -- duplicate last element of the stack
+dup  :: [Int] -> [Int]
 dup [] = [0]
 dup s  = push s (last s)
+
 -- add the last two elements of the stack
+add  :: [Int] -> [Int]
 add []  = [0]
 add [x] = [x]
 add s   = push (pop (pop s)) ((last s) + (last (pop s)))
+
 -- subtract second last element from last element
+sub  :: [Int] -> [Int]
 sub [] = [0]
 sub s  = add (push (neg (pop s)) (last s))
+
 -- multiply last two elements of the stack
+mul  :: [Int] -> [Int]
 mul []  = [0]
 mul [x] = [0]
 mul s   = push (pop (pop s)) ((last s) * (last (pop s)))
+
 -- negate the last element of the stack
+neg  :: [Int] -> [Int]
 neg [] = [0]
 neg s  = push (pop s) (-(last s))
+
 -- push the given number on the stack
+push :: [Int] -> Int -> [Int]
 push s x = s ++ [x]
 
 -- read text commands
+readCommand :: String -> [Int] -> [Int]
 readCommand "pop" s = pop s
 readCommand "dup" s = dup s
 readCommand "add" s = add s
