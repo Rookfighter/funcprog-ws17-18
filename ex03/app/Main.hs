9e245fdb63ef325d938c64ffb53be3388f71eaa2
Fabian Meyer <user8324@posteo.net>
2017-11-29 16:13:54 +0100

    Implement dragon curve

--- ex03/app/Main.hs
+++ ex03/app/Main.hs
@@ -7,12 +7,14 @@ import qualified Vec2 as V
 import qualified Picture as P
 import qualified PictureSvg as Ps
 import Graphics.Svg
+import DragonCurve
 
 
 houses = [P.square_house, P.square_house, P.square_house, P.square_house, P.square_house]
-contents g = Ps.draw_picture . foldr rand_house mempty $ houses
+housings g = Ps.draw_picture . foldr rand_house mempty $ houses
     where rand_house = scale_house (random g)
           scale_house (s, _) h = P.move (V.Vec2 (s*20) 0) . mappend (P.move (V.Vec2 0 (20.0-s)) . P.scale (s*20.0) $ h)
+
 
 svg :: Element -> Element
 svg content = doctype <>
@@ -24,4 +26,4 @@ svg content = doctype <>
 main :: IO ()
 main = do
     g <- getStdGen
-    renderToFile "foobar.svg" (svg . Ps.draw_picture . P.rotate (V.Vec2 50 50) (pi) . P.move (V.Vec2 40 0) . P.scale 20 $ P.square_house)
+    renderToFile "foobar.svg" (svg . Ps.draw_picture . P.move (V.Vec2 40 70) . P.scale 2 . dragon' $ 15)
