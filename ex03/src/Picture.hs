34fd56349eeb6411c832be37393932a190b5e8a3
Fabian Meyer <user8324@posteo.net>
2017-11-29 10:09:53 +0100

    Define Line and Polygon with arbitrary number of points

--- ex03/src/Picture.hs
+++ ex03/src/Picture.hs
@@ -7,13 +7,12 @@ module Picture where
 
 import qualified Vec2 as V
 
--- helper typedefs for making code more readable
+-- Point type to make code more readable
 type Point = V.Vec2 Double
-type Dim = V.Vec2 Double
 
 -- picture datatype which allows to compose several shapes
-data Picture = Blank | Line Point Point | Rect Point Dim | Circle Point Double |
-    Child Picture Picture
+data Picture = Blank | Line [Point] | Pol [Point] |
+    Circle Point Double | Comp Picture Picture
     deriving (Show)
 
 -- implement monoid typeclass for Picture
@@ -21,27 +20,28 @@ instance Monoid Picture where
     mempty = Blank
     mappend p Blank = p
     mappend Blank p = p
-    mappend p1 p2 = Child p1 p2
+    mappend p1 p2 = Comp p1 p2
 
 -- scale given picture by a factor
 scale :: Double -> Picture -> Picture
+-- scale Blank simply returns Blank
 scale fac Blank = Blank
-scale fac (Line p1 p2) = Line (V.scale fac p1) (V.scale fac p2)
-scale fac (Rect p d) = Rect (V.scale fac p) (V.scale fac d)
+-- scale Line
+scale fac (Line ps) = Line . map (V.scale fac) $ ps
+-- scale Polygon
+scale fac (Pol ps) = Pol . map (V.scale fac) $ ps
+-- scale Circle
 scale fac (Circle c r) = Circle (V.scale fac c) (r*fac)
-scale fac (Child p1 p2) = Child (scale fac p1) (scale fac p2)
+-- scale nested Pictures
+scale fac (Comp p1 p2) = Comp (scale fac p1) (scale fac p2)
 
 -- define sqare triangle picture from ex sheet
-square_tri = Child
-                (Rect (V.Vec2 0 0) (V.Vec2 1 1))
-                (Child
-                    (Line (V.Vec2 0.5 0) (V.Vec2 0 1))
-                    (Line (V.Vec2 0.5 0) (V.Vec2 1 1)))
+square_tri = mappend (Pol [V.Vec2 0 0, V.Vec2 1 0, V.Vec2 1 1, V.Vec2 0 1])
+             (Line [V.Vec2 0 1, V.Vec2 0.5 0, V.Vec2 1 1])
 
 -- define square house with roof window from ex sheet
-square_house = mappend (Rect (V.Vec2 0 1) (V.Vec2 1 1)) .
-               mappend (Line (V.Vec2 0.5 0) (V.Vec2 0 1)) .
-               mappend (Line (V.Vec2 0.5 0) (V.Vec2 1 1)) .
-               mappend (Line (V.Vec2 1 1) (V.Vec2 0 2)) .
-               mappend (Line (V.Vec2 0 1) (V.Vec2 1 2)) $
+square_house = mappend (Pol [V.Vec2 0 1, V.Vec2 1 1, V.Vec2 1 2, V.Vec2 0 2]) .
+               mappend (Line [V.Vec2 0 1, V.Vec2 0.5 0, V.Vec2 1 1]) .
+               mappend (Line [V.Vec2 1 1, V.Vec2 0 2]) .
+               mappend (Line [V.Vec2 0 1, V.Vec2 1 2]) $
                Circle (V.Vec2 0.5 0.70) 0.25
