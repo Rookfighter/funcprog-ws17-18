38dbdd72236497d77657cd8f311681cf4d2a001c
Fabian Meyer <user8324@posteo.net>
2017-12-08 18:08:55 +0100

    Add tests for ex04.3

--- test/ex04/TestFold.hs
+++ test/ex04/TestFold.hs
@@ -6,18 +6,27 @@
 module TestFold where
 
 import Fold
+import Data.List
 import Test.QuickCheck
 
-prop_or :: [Bool] -> Bool
-prop_or xs = or xs == or' xs
+prop_or' :: [Bool] -> Bool
+prop_or' xs = or xs == or' xs
 
-prop_filter :: [Int] -> Int -> Bool
-prop_filter xs c = filter (>c) xs == filter' (>c) xs
+prop_filter' :: [Int] -> Int -> Bool
+prop_filter' xs c = filter (>c) xs == filter' (>c) xs
 
-prop_map :: [Int] -> Int -> Bool
-prop_map xs a = map (*a) xs == map' (*a) xs
+prop_map' :: [Int] -> Int -> Bool
+prop_map' xs a = map (*a) xs == map' (*a) xs
+
+prop_map'' :: [Int] -> Int -> Bool
+prop_map'' xs a = map (*a) xs == map'' (*a) xs
+
+prop_iterate' :: Int -> Int -> Bool
+prop_iterate' a l = (take l . iterate (*2) $ a) == (take l . iterate' (*2) $ a)
 
 test_Fold = do
-    quickCheck prop_or
-    quickCheck prop_filter
-    quickCheck prop_map
+    quickCheck prop_or'
+    quickCheck prop_filter'
+    quickCheck prop_map'
+    quickCheck prop_map''
+    quickCheck prop_iterate'
