f4c083bc75f1bace0882588247e0a8c0c188c907
Fabian Meyer <user8324@posteo.net>
2018-01-15 13:58:24 +0100

    Add timestamps to logging

--- src/ex07/StructuredLogging.hs
+++ src/ex07/StructuredLogging.hs
@@ -5,24 +5,32 @@
 
 module StructuredLogging where
 
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Writer.Strict
+import Data.Time.Clock.POSIX
 
-data Item = Msg String
-          | Section String [Item]
+data Item = Msg POSIXTime String
+          | Section POSIXTime POSIXTime String [Item]
     deriving (Show,Eq)
 
 type Log = [Item]
-type Logging a = Writer Log a
+type Logging a = WriterT Log IO a
 
 -- logs the messages s
 log :: Show t => t -> Logging ()
-log = tell . return . Msg . show
+log s = do
+    ts <- lift $ getPOSIXTime
+    tell $ return $ Msg ts (show s)
 
--- executes m and add its log in a section titled ‘s‘.
+-- executes m and add its log in a section titled s
 withSection :: String -> Logging a -> Logging a
-withSection s m =
-    censor (\l -> return $ Section s l)  m
+withSection s m = do
+    ts1 <- lift $ getPOSIXTime
+    a <- m
+    ts2 <- lift $ getPOSIXTime
+    pass $ return (a, \l -> return $ Section ts1 ts2 s l)
+
 
 -- run logger to retrieve the result
-runLogging :: Logging a -> (a, Log)
-runLogging l = runWriter l
+runLogging :: Logging a -> IO (a, Log)
+runLogging l = runWriterT l
