d51d8641f5e4a254b19d9c83aec869128dfb9ad5
Fabian Meyer <user8324@posteo.net>
2018-02-27 14:35:51 +0100

    Implement safe list

--- src/ex08/SafeList.hs
+++ src/ex08/SafeList.hs
@@ -6,11 +6,30 @@
 
 module SafeList where
 
-data SafeList a where
-    Nil  :: SafeList ()
-    Cons :: a -> SafeList a -> SafeList a
+data Empty;
+data NonEmpty;
+
+data SafeList a b where
+    Nil  :: SafeList a Empty
+    Cons :: a -> SafeList a b -> SafeList a NonEmpty
 
 
-safeHead = undefined
-safeDrop = undefined
-safeAppend = undefined
+innerStr :: Show a => SafeList a b -> String
+innerStr (Cons x (Cons y ys)) = show x ++ "," ++ innerStr (Cons y ys)
+innerStr (Cons x Nil)         = show x
+
+instance Show a => Show (SafeList a b) where
+    show Nil = "[]"
+    show xs  = "[" ++ innerStr xs ++ "]"
+
+safeHead :: SafeList a NonEmpty -> a
+safeHead (Cons x _) = x
+
+
+safeDrop :: Int -> SafeList a NonEmpty -> SafeList a NonEmpty
+safeDrop i (Cons x (Cons y ys))  | i > 0 = safeDrop (i-1) (Cons y ys)
+safeDrop i (Cons x xs)                   = Cons x xs
+
+safeAppend :: SafeList a NonEmpty -> SafeList a NonEmpty -> SafeList a NonEmpty
+safeAppend (Cons x Nil) ys = Cons x ys
+safeAppend (Cons x (Cons y ys)) zs = Cons x $ safeAppend (Cons y ys) zs
