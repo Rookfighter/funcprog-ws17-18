eceb3c481dc7d9ca8dd4a271f78a28d4bb15096a
Fabian Meyer <user8324@posteo.net>
2018-02-28 11:28:56 +0100

    Fix unsafe operations on SProg

--- src/ex08/SafeStackCalc.hs
+++ src/ex08/SafeStackCalc.hs
@@ -10,27 +10,22 @@ data Empty;
 data NonEmpty;
 data None;
 
--- TODO these cause runtime errors but should actually not compile
-test1 = eval $ And (Leq (Push 2 (Push 3 Nil)))
-test2 = eval $ Add (Add (Push 2 Nil))
-test3 = eval $ Pop (Pop (Pop (Push 2 Nil)))
-
 data SProg a b c where
     Nil  :: SProg Int None Empty
     Noop :: SProg a b c -> SProg a b c
-    Pop  :: SProg a b NonEmpty -> SProg b c d
-    Push :: Int -> SProg b c d -> SProg Int b NonEmpty
-    Dup  :: SProg a b c -> SProg a a NonEmpty
-    Dup2 :: SProg a b c -> SProg a b NonEmpty
-    Flip :: SProg a b NonEmpty -> SProg a b NonEmpty
-    Add  :: SProg Int Int NonEmpty -> SProg Int a NonEmpty
-    Sub  :: SProg Int Int NonEmpty -> SProg Int a NonEmpty
-    Mul  :: SProg Int Int NonEmpty -> SProg Int a NonEmpty
-    Leq  :: SProg Int Int NonEmpty -> SProg Bool a NonEmpty
-    Geq  :: SProg Int Int NonEmpty -> SProg Bool a NonEmpty
+    Pop  :: SProg a (SProg b c d) NonEmpty -> SProg b c d
+    Push :: Int -> SProg b c d -> SProg Int (SProg b c d) NonEmpty
+    Dup  :: SProg a b c -> SProg a (SProg a b c) NonEmpty
+    Dup2 :: SProg a b c -> SProg a (SProg b (SProg a b c) NonEmpty) NonEmpty
+    Flip :: SProg a (SProg b c d) NonEmpty -> SProg b (SProg a c d) NonEmpty
+    Add  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
+    Sub  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
+    Mul  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
+    Leq  :: SProg Int (SProg Int a b) NonEmpty -> SProg Bool a NonEmpty
+    Geq  :: SProg Int (SProg Int a b) NonEmpty -> SProg Bool a NonEmpty
     Not  :: SProg Bool a NonEmpty -> SProg Bool a NonEmpty
-    And  :: SProg Bool Bool NonEmpty -> SProg Bool a NonEmpty
-    Or   :: SProg Bool Bool NonEmpty -> SProg Bool a NonEmpty
+    And  :: SProg Bool (SProg Bool a b) NonEmpty -> SProg Bool a NonEmpty
+    Or   :: SProg Bool (SProg Bool a b) NonEmpty -> SProg Bool a NonEmpty
     Seq  :: SProg a b c -> SProg d e f -> SProg d e f
     If   :: SProg Bool a NonEmpty -> SProg b c d -> SProg b c d -> SProg b c d
 
@@ -60,21 +55,21 @@ instance Show Value where
     show (I a) = show a
     show (B b) = show b
 
-evalAritOp :: (Int -> Int -> Int) -> SProg Int Int NonEmpty -> [Value]
+evalAritOp :: (Int -> Int -> Int) -> SProg Int (SProg Int a b) NonEmpty -> [Value]
 evalAritOp op s =
     let xs    = eval s
         (I a) = head xs
         (I b) = head $ tail xs
     in (I $ a `op` b) : (tail $ tail xs)
 
-evalCompOp :: (Int -> Int -> Bool) -> SProg Int Int NonEmpty -> [Value]
+evalCompOp :: (Int -> Int -> Bool) -> SProg Int (SProg Int a b) NonEmpty -> [Value]
 evalCompOp op s =
     let xs    = eval s
         (I a) = head xs
         (I b) = head $ tail xs
     in (B $ a `op` b) : (tail $ tail xs)
 
-evalLogicOp :: (Bool -> Bool -> Bool) -> SProg Bool Bool NonEmpty -> [Value]
+evalLogicOp :: (Bool -> Bool -> Bool) -> SProg Bool (SProg Bool a b) NonEmpty -> [Value]
 evalLogicOp op s =
     let xs    = eval s
         (B a) = head xs
