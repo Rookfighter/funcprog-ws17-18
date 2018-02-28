443946c1eb4378795216ac39dac99e1b525eec28
Fabian Meyer <user8324@posteo.net>
2018-02-28 11:33:42 +0100

    Remove unneeded type param

--- src/ex08/SafeStackCalc.hs
+++ src/ex08/SafeStackCalc.hs
@@ -6,30 +6,28 @@
 
 module SafeStackCalc where
 
-data Empty;
-data NonEmpty;
 data None;
 
-data SProg a b c where
-    Nil  :: SProg Int None Empty
-    Noop :: SProg a b c -> SProg a b c
-    Pop  :: SProg a (SProg b c d) NonEmpty -> SProg b c d
-    Push :: Int -> SProg b c d -> SProg Int (SProg b c d) NonEmpty
-    Dup  :: SProg a b c -> SProg a (SProg a b c) NonEmpty
-    Dup2 :: SProg a b c -> SProg a (SProg b (SProg a b c) NonEmpty) NonEmpty
-    Flip :: SProg a (SProg b c d) NonEmpty -> SProg b (SProg a c d) NonEmpty
-    Add  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
-    Sub  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
-    Mul  :: SProg Int (SProg Int a b) NonEmpty -> SProg Int a NonEmpty
-    Leq  :: SProg Int (SProg Int a b) NonEmpty -> SProg Bool a NonEmpty
-    Geq  :: SProg Int (SProg Int a b) NonEmpty -> SProg Bool a NonEmpty
-    Not  :: SProg Bool a NonEmpty -> SProg Bool a NonEmpty
-    And  :: SProg Bool (SProg Bool a b) NonEmpty -> SProg Bool a NonEmpty
-    Or   :: SProg Bool (SProg Bool a b) NonEmpty -> SProg Bool a NonEmpty
-    Seq  :: SProg a b c -> SProg d e f -> SProg d e f
-    If   :: SProg Bool a NonEmpty -> SProg b c d -> SProg b c d -> SProg b c d
+data SProg a b where
+    Nil  :: SProg Int None
+    Noop :: SProg a b -> SProg a b
+    Pop  :: SProg a (SProg b c) -> SProg b c
+    Push :: Int -> SProg b c -> SProg Int (SProg b c)
+    Dup  :: SProg a b -> SProg a (SProg a b)
+    Dup2 :: SProg a b -> SProg a (SProg b (SProg a b))
+    Flip :: SProg a (SProg b c) -> SProg b (SProg a c)
+    Add  :: SProg Int (SProg Int a) -> SProg Int a
+    Sub  :: SProg Int (SProg Int a) -> SProg Int a
+    Mul  :: SProg Int (SProg Int a) -> SProg Int a
+    Leq  :: SProg Int (SProg Int a) -> SProg Bool a
+    Geq  :: SProg Int (SProg Int a) -> SProg Bool a
+    Not  :: SProg Bool a -> SProg Bool a
+    And  :: SProg Bool (SProg Bool a) -> SProg Bool a
+    Or   :: SProg Bool (SProg Bool a) -> SProg Bool a
+    Seq  :: SProg a b -> SProg c d -> SProg c d
+    If   :: SProg Bool a -> SProg b c -> SProg b c -> SProg b c
 
-instance Show (SProg a b c) where
+instance Show (SProg a b) where
     show Nil          = "Nil"
     show (Noop s)     = "Noop (" ++ (show s) ++ ")"
     show (Pop s)      = "Pop ("  ++ (show s) ++ ")"
@@ -55,28 +53,28 @@ instance Show Value where
     show (I a) = show a
     show (B b) = show b
 
-evalAritOp :: (Int -> Int -> Int) -> SProg Int (SProg Int a b) NonEmpty -> [Value]
+evalAritOp :: (Int -> Int -> Int) -> SProg Int (SProg Int a) -> [Value]
 evalAritOp op s =
     let xs    = eval s
         (I a) = head xs
         (I b) = head $ tail xs
     in (I $ a `op` b) : (tail $ tail xs)
 
-evalCompOp :: (Int -> Int -> Bool) -> SProg Int (SProg Int a b) NonEmpty -> [Value]
+evalCompOp :: (Int -> Int -> Bool) -> SProg Int (SProg Int a) -> [Value]
 evalCompOp op s =
     let xs    = eval s
         (I a) = head xs
         (I b) = head $ tail xs
     in (B $ a `op` b) : (tail $ tail xs)
 
-evalLogicOp :: (Bool -> Bool -> Bool) -> SProg Bool (SProg Bool a b) NonEmpty -> [Value]
+evalLogicOp :: (Bool -> Bool -> Bool) -> SProg Bool (SProg Bool a) -> [Value]
 evalLogicOp op s =
     let xs    = eval s
         (B a) = head xs
         (B b) = head $ tail xs
     in (B $ a `op` b) : (tail $ tail xs)
 
-eval :: SProg a b c -> [Value]
+eval :: SProg a b -> [Value]
 eval Nil         = [I 0]
 eval (Noop s)    = eval s
 eval (Pop s)     = tail $ eval s
