8f51320b76add9181dc2709e3265b10f21ebfc4c
Fabian Meyer <user8324@posteo.net>
2018-03-01 10:25:20 +0100

    Implement Seq, While, If

--- src/ex08/SafeStackCalc.hs
+++ src/ex08/SafeStackCalc.hs
@@ -8,24 +8,28 @@ module SafeStackCalc where
 
 data None;
 
+exIf = eval $ If (Geq) (\s -> Add (Push 10 s)) (\s -> Add (Push (-10) s)) (Push 25 (Push 95 Nil))
+exWhile = eval $ While (Leq) (\s -> Add (Push 1 s)) (Push (-10) Nil)
+
 data SProg a b where
-    Nil  :: SProg Int None
-    Noop :: SProg a b -> SProg a b
-    Pop  :: SProg a (SProg b c) -> SProg b c
-    Push :: Int -> SProg b c -> SProg Int (SProg b c)
-    Dup  :: SProg a b -> SProg a (SProg a b)
-    Dup2 :: SProg a b -> SProg a (SProg b (SProg a b))
-    Flip :: SProg a (SProg b c) -> SProg b (SProg a c)
-    Add  :: SProg Int (SProg Int a) -> SProg Int a
-    Sub  :: SProg Int (SProg Int a) -> SProg Int a
-    Mul  :: SProg Int (SProg Int a) -> SProg Int a
-    Leq  :: SProg Int (SProg Int a) -> SProg Bool a
-    Geq  :: SProg Int (SProg Int a) -> SProg Bool a
-    Not  :: SProg Bool a -> SProg Bool a
-    And  :: SProg Bool (SProg Bool a) -> SProg Bool a
-    Or   :: SProg Bool (SProg Bool a) -> SProg Bool a
-    Seq  :: SProg a b -> SProg c d -> SProg c d
-    If   :: SProg Bool a -> SProg b c -> SProg b c -> SProg b c
+    Nil   :: SProg Int None
+    Noop  :: SProg a b -> SProg a b
+    Pop   :: SProg a (SProg b c) -> SProg b c
+    Push  :: Int -> SProg b c -> SProg Int (SProg b c)
+    Dup   :: SProg a b -> SProg a (SProg a b)
+    Dup2  :: SProg a b -> SProg a (SProg b (SProg a b))
+    Flip  :: SProg a (SProg b c) -> SProg b (SProg a c)
+    Add   :: SProg Int (SProg Int a) -> SProg Int a
+    Sub   :: SProg Int (SProg Int a) -> SProg Int a
+    Mul   :: SProg Int (SProg Int a) -> SProg Int a
+    Leq   :: SProg Int (SProg Int a) -> SProg Bool a
+    Geq   :: SProg Int (SProg Int a) -> SProg Bool a
+    Not   :: SProg Bool a -> SProg Bool a
+    And   :: SProg Bool (SProg Bool a) -> SProg Bool a
+    Or    :: SProg Bool (SProg Bool a) -> SProg Bool a
+    Seq   :: SProg a b -> (SProg a b -> SProg c d) -> SProg c d
+    If    :: (SProg a b -> SProg Bool c) -> (SProg a b -> SProg d e) -> (SProg a b -> SProg d e) -> SProg a b -> SProg d e
+    While :: (SProg a b -> SProg Bool c) -> (SProg a b -> SProg a b) -> SProg a b -> SProg a b
 
 instance Show (SProg a b) where
     show Nil          = "Nil"
@@ -43,8 +47,9 @@ instance Show (SProg a b) where
     show (Not s)      = "Not ("  ++ (show s) ++ ")"
     show (And s)      = "And ("  ++ (show s) ++ ")"
     show (Or s)       = "Or ("  ++ (show s) ++ ")"
-    show (Seq s1 s2)  = "Seq (" ++ (show s1) ++ ") (" ++ (show s2) ++ ")"
-    show (If c s1 s2) = "If (" ++ (show c) ++ ") (" ++ (show s1) ++ ") (" ++ (show s2) ++ ")"
+    --show (Seq s1 s2)  = undefined -- "Seq (" ++ (show s1) ++ ") (" ++ (show s2) ++ ")"
+    --show (If c s1 s2) = undefined -- "If (" ++ (show c) ++ ") (" ++ (show s1) ++ ") (" ++ (show s2) ++ ")"
+    show _  = undefined -- "While (foo) (" ++ (show s) ++ ")"
 
 data Value = I Int
            | B Bool
@@ -105,8 +110,12 @@ eval (Not s) =
     in (B $ not x) : (tail xs)
 eval (And s) = evalLogicOp (&&) s
 eval (Or s)  = evalLogicOp (||) s
-eval (Seq s1 s2)  = (eval s2) ++ (eval s1)
-eval (If c s1 s2) =
-    let xs    = eval c
+eval (Seq s1 fs2) = eval $ fs2 s1
+eval (If cond branch1 branch2 s0) =
+    let xs    = eval $ cond s0
         (B b) = head xs
-    in if b then eval s1 else eval s2
+    in if b then eval (Seq s0 branch1) else eval (Seq s0 branch2)
+eval (While cond body s0) =
+    let xs = eval $ cond s0
+        (B b) = head xs
+    in if b then eval (While cond body $ Seq s0 body) else eval s0
